07d1ff9e936ee5ec92a59da3da1f1a86f1841055
Brian Holbrook <brian.holbrook56@gmail.com>
2022-02-15 15:49:06 -0600

    we done

---
+++ README copy.md
@@ -0,0 +1,39 @@
+Welcome to my Portfolio.
+========================
+
+this webpage is complete with a simple header, main, and footer.
+
+Header
+------
+
+on the header is a title and navigation bar that breaks down into a navigation
+menu. On the menu are three destinations which are the three sections of the
+page: About Me, Contact Me, and My Work
+
+Main
+----
+
+# about me section #
+
+The main section includes a picture of me and my girlfriend. To the right of that are three large texts serving as an introduction to me. Underneath is a break and a paragraph describing my beginning journey into coding. At the end of the paragraph is a link to my resume.
+
+# contact me section #
+
+The contact me section is a small bar cutting across the screen with interactive buttons displaying the logos of the different ways to contact me.
+
+# my work section #
+
+this section contains several placeholder images that will remind you they're placeholders when you click on them. the only link that works is my first project that i just finished yesterday(rock paper scissors).
+
+footer
+------
+
+the footer only has a brief message thanking the user for visiting the page.
+
+#  url #   
+
+A [Link](https://holbrookb23.github.io/myPortfolio/)
+
+# screenshot of page #
+
+! [Image](./assets/img/screenshot.png)
---
+++ assets/script.js
@@ -0,0 +1,40 @@
+const projects = [
+    {
+        name: "Weather Week",
+        deployed: "https://holbrookb23.github.io/weatherWeek/",
+        repo: "https://github.com/holbrookb23/weatherWeek",
+        img: "./assets/img/projects/weatherWeek2.png",
+    },
+    {
+        name: "Quiz Game",
+        deployed: "https://holbrookb23.github.io/quizGame/",
+        repo: "https://github.com/holbrookb23/quizGame",
+        img: "./assets/img/projects/quizGame.png",
+    },
+    {
+        name: "Best Shot",
+        deployed: "https://thundercats-4ever.github.io/project/",
+        repo: "https://github.com/thundercats-4ever/project",
+        img: "./assets/img/projects/bestShot.png",
+    },
+];
+
+
+
+
+
+projects.forEach((project) =>{
+    const projectCard = document.createElement("div");
+    projectCard.classList.add("projectCard");
+    projectCard.innerHTML = `
+    <img src="${project.img}" alt="" class="br-1 w-100">
+    <div class="b br-1 bg-tw z-1 p-a w-100">
+        <h4 class="ta-c bb p">${project.name}</h4>
+        <div class="df jc-se b-0">
+            <button class="bo bblr pt-3 pb-5 projectBtn"><a href="${project.deployed}" target="_blank" class="c-black td-n w-100">Link</a></button>  
+            <button class="bo bbrr pt-3 pb-5 projectBtn"><a href="${project.repo}" target="_blank" class="c-black td-n w-100">Repo</a></button>
+        </div>
+    </div>`;
+
+    document.getElementById("projects").append(projectCard);
+})
